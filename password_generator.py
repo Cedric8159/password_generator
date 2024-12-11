dd9e461080496b6cec5ebaa625460dfb66c3df38
Cedric8159 <irambonacedric55@gmail.com>
2024-12-10 19:36:23 -0600

    Initial commit: Set up project directory and file structure.

---
+++ password_generator.py
@@ -0,0 +1,10 @@
+"""
+Password Generator Script
+
+This script generates secure, random passwords based on user preferences.
+Users can specify the length of the password and choose to include letters which
+consists of uppercase and lowercase letters, numbers, and special characters.
+
+Author: Cedrick
+Date: December 10, 2024
+"""
