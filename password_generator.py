507835ff48f3d28936098b7aa7731f88ae63abc2
Cedric8159 <cedric.sebineza@gmail.com>
2024-12-10 20:40:33 -0600

    Added lists for letters, numbers and symbols.

--- password_generator.py
+++ password_generator.py
@@ -8,3 +8,15 @@ consists of uppercase and lowercase letters, numbers, and special characters.
 Author: Cedrick
 Date: December 10, 2024
 """
+import random
+
+# Creating separate lists for letters, numbers, and special characters
+upper_lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+           'm', 'n', 'o', 'p', 'r', 's', 't', 'u' 'v', 'w', 'x', 'y', 
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+symbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-',
+            '?', ':', ';', ',', '.', '[', ']', '{', '}', '|', '_', '>', '<']
